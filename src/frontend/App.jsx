9a3e17c3c90b3942c27f9fd83b6fa14e777b876d
Rave JS <hswatter@mail.ru>
2024-01-14 17:49:13 +0300

    l

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,7 +1,11 @@
-export default function App() {
+import React from 'react'
+
+function App() {
   return (
     <div>
       <h1>Hello, React!</h1>
     </div>
   )
 }
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import '@/styles/index.scss'
-import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './frontend/App'
+import '@/styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
