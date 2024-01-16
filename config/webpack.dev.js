db560a10a169ea4a8266f30d606b9020fff18f65
Rave JS <hswatter@mail.ru>
2024-01-16 15:45:43 +0300

    config for react, scss.modules

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -26,7 +26,7 @@ module.exports = merge(common, {
           'style-loader',
           {
             loader: 'css-loader',
-            options: { sourceMap: true, importLoaders: 1, modules: false },
+            options: { sourceMap: true, importLoaders: 1, modules: true },
           },
           { loader: 'postcss-loader', options: { sourceMap: true } },
           { loader: 'sass-loader', options: { sourceMap: true } },
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section>
+          <h3>Something new</h3>
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default App
