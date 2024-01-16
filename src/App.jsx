8007ad60fcf796c8c525981404a573f32a994a77
raves <hswatter@mail.ru>
2024-01-16 19:48:58 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 import React from 'react'
 import Header from './components/Header'
-import { info } from './data'
-
-function SocialInfo(props) {
-  return (
-    <li>
-      <p>
-        <strong>sf</strong>ss
-      </p>
-    </li>
-  )
-}
 
 function App() {
   return (
     <div>
       <Header />
-      <main>
-        <section>
-          <ul>
-            <SocialInfo title="111" description="js" />
-            <SocialInfo title="222" description="css" />
-            <SocialInfo title="333" description="react" />
-          </ul>
-        </section>
-      </main>
     </div>
   )
 }
